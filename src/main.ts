389539bb57424fc7aaa66cfa1e9939f3511f708a
Anton <deragandi0@gmail.com>
2025-06-10 20:39:52 +0300

    added crypto to the global packages

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import { ConfigService } from '@nestjs/config';
+import crypto from 'crypto';
+
+if (!global.crypto) {
+  global.crypto = crypto as any;
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
